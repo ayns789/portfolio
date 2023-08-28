ced72e129cfb1ef64c963bfc698c804842bf4b60
github_ayns789 <joly.a.idf@gmail.com>
2023-08-28 18:16:42 +0200

    ✨ MAINPROJECT > create section text and add styles

--- src/components/MainProject/index.jsx
+++ src/components/MainProject/index.jsx
@@ -1,9 +1,16 @@
 import style from './index.module.css';
+import TextSectionMain from '../TextSectionMain';
 
 export default function MainProject() {
   return (
     <>
-      <div className={style.layout}></div>
+      <div className={style.layout}>
+        <div className='leftSection'>
+          <h2>Projet développé dans les conditions d'entreprise</h2>
+          <TextSectionMain />
+        </div>
+        <div className='rightSection'></div>
+      </div>
     </>
   );
 }
